c897ddb5f03ea16ea2625b645ae0a162ce1cf9c6
vespertino <vespertino@ii23pc05>
2024-04-18 16:28:03 +0200

    Primer commit de Eclipse para Github

---
+++ Github/src/principal/clase.java
@@ -0,0 +1,7 @@
+package principal;
+
+public class clase {
+	public void holamundo() {
+		System.out.println("!Hola Mundo!");
+	}
+}
---
+++ Github/src/principal/main.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class main {
+
+	public static void main(String[] args) {
+		clase objClase = new clase();		
+		objClase.holamundo();
+		
+	}
+
+}
